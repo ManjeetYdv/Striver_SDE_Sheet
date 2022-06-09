bbbdd1c0e01c1315508f2b2fc08c503f26d50619
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-09 23:14:57 +0530

    Update merge2sortedLists.java

--- LinkedList/merge2sortedLists.java
+++ LinkedList/merge2sortedLists.java
@@ -1,116 +1,44 @@
-package com.company;
-
-public class LL {
-
-    private Node head;
-    private Node tail;
-    private int size;
-
-    public LL() {
-         this.size = 0;
-    }
-
-    private class Node{
-        private int value;
-        private Node next;
-
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public Node(int value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
-
-    public void InsertAtFirst(int value){                      //Insert At First
-        Node node = new Node(value);
-        node.next=head;
-        head= node;
-
-        if(tail==null){                //when first element is inserted.
-            tail=head;
-        }
-        size++;
-
-    }
-
-    public void InsertAtLast(int value){            //Insert At Last
-        Node node = new Node(value);                //advantage of maintaining tail is here this takes 0(1) time
-        if(tail==null){
-            InsertAtFirst(value);
-            return;
-        }
-        tail.next= node;
-        tail = node;
-        size++;
-
-
-    }
-   
-
-
-    public void display(){
-        Node temp = head;
-
-        while(temp!=null){
-            System.out.print(temp.value+" -> " );
-            temp =temp.next;
-        }
-        System.out.println("END");
-
-    }
-
-    
-    public static LL merge(LL list1 , LL list2){
-        Node p1 = list1.head;
-        Node p2 = list2.head;
-
-        LL merged = new LL();
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        
+        ListNode head = new ListNode();
+        ListNode curr =head;
         while(p1!=null && p2!=null){
-
-            if(p1.value<p2.value){
-                merged.InsertAtLast(p1.value);
-                p1 = p1.next;
+            if(p1.val<p2.val){
+                curr.next = p1;
+                p1=p1.next;
+                curr= curr.next;
             }
-           else{
-                merged.InsertAtLast(p2.value);
+            else {
+                curr.next =p2;
                 p2 =p2.next;
+                curr=curr.next;
+                
             }
         }
-
         while(p1!=null){
-         merged.InsertAtLast(p1.value);
-         p1 = p1.next;
-
+              curr.next = p1;
+               p1=p1.next;
+               curr= curr.next;
         }
         while(p2!=null){
-            merged.InsertAtLast(p2.value);
-            p2 = p2.next;
+            curr.next =p2;
+             p2 =p2.next;
+             curr=curr.next;
         }
-
-        return merged;
+        return head.next;
     }
-
-
-    public static void main(String[] args) {
-        
-        LL list1 = new LL();
-        list1.InsertAtLast(1);
-        list1.InsertAtLast(2);
-        list1.InsertAtLast(5);
-        list1.InsertAtLast(7);
-
-        LL list2 = new LL();
-        list2.InsertAtLast(3);
-        list2.InsertAtLast(4);
-        list2.InsertAtLast(9);
-
-       LL merged = new LL();
-       merged = LL.merge(list1 , list2);
-       merged.display();
-
-    }
-
 }
