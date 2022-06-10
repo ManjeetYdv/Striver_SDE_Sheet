773ac286989530660984a65a0821afa5398b0e8b
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-10 23:16:37 +0530

    Create LargestSubarrWithSum_.java

---
+++ Arrays Part-4/LargestSubarrWithSum_.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class MaxSubarrayWithSum_k {
+
+    public static void main(String[] args) {
+
+        int[] arr ={1,2,3};
+        int k=3;
+
+        int maxLen=0;
+        int currLen=0;
+        int prefix_sum =0;
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        for(int i=0 ;i<arr.length ;i++){
+
+            prefix_sum +=arr[i];
+
+            if(arr[i]==k && maxLen==0){
+                maxLen =1;
+            }
+            if(prefix_sum ==k ){
+                maxLen = i+1;
+            }
+
+            if(map.containsKey(prefix_sum-k)){
+
+                maxLen = Integer.max(maxLen , i-map.get(prefix_sum-k));
+            }
+            else{
+
+                map.put(prefix_sum , i);
+            }
+        }
+
+        System.out.println(maxLen);
+    }
+}
