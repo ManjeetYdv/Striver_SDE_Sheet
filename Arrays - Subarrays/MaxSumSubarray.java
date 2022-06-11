2e2ff20f5de0cc85ba192d2ae36db202bc54e933
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-11 08:51:57 +0530

    Q 53 leetcode

--- Arrays - Subarrays/MaxSumSubarray.java
+++ Arrays - Subarrays/MaxSumSubarray.java
@@ -1,10 +1,7 @@
-package com.company;
-
-public class kadane {
-
-    public static int kadaneAlgo(int [] arr){
-        int currSum =arr[0];
-        int maxSum = arr[0];
+class Solution {
+    public int maxSubArray(int[] arr) {
+         int currSum =arr[0];
+         int maxSum = arr[0];
 
         for(int i=1; i<arr.length ;i++){
             if(currSum>=0){
@@ -16,10 +13,5 @@ public class kadane {
             maxSum = Integer.max(currSum , maxSum);
         }
         return maxSum;
-    }
-
-    public static void main(String[] args) {
-        int[] arr={-9 , -8 ,2  , 5};
-        System.out.println(kadaneAlgo(arr));
     }
 }
