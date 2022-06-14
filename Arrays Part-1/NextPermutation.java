c251ae034cf4d986eebc2fc4687591f8877ef43c
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-14 16:17:04 +0530

    leetcode 31

--- Arrays Part-1/NextPermutation.java
+++ Arrays Part-1/NextPermutation.java
@@ -1,69 +1,51 @@
-package com.company;
-
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.Vector;
-
-public class NextPermutation {
-    public static void nextPermutation(Vector<Integer> vector){
-
-        int size= vector.size();
-        int idx=-1;
-        for(int i= vector.size()-1; i>=1 ;i--){
-            if(vector.get(i)> vector.get(i-1)){
-                idx =i;
+class Solution {
+    
+   public static void revereseArray(int arr[], int start, int end) {
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+       return;
+       } 
+    
+    public void swap(int arr[] , int x , int y){
+        int temp = arr[y];
+        arr[y]=arr[x];
+        arr[x] = temp;
+        return;
+    }
+    public void nextPermutation(int[] arr) {
+        
+        
+        int idx =-1;
+        for(int i=arr.length-1 ;i>=1 ;i--){
+            
+            if(arr[i]>arr[i-1]){
+                idx = i;
                 break;
             }
         }
-
+        
         if(idx==-1){
-            Collections.reverse(vector);
+            revereseArray(arr , 0 , arr.length-1);
             return;
         }
-        else{
-            int prev=idx;
-            for(int i=prev+1 ;i<vector.size();i++){
-                if(vector.get(i)> vector.get(idx-1) && vector.get(i)<=vector.get(prev)){
-                    prev=i;
-                }
+        
+        int prev =idx;
+        
+        for(int i=idx ;i<arr.length ;i++){
+            
+            if(arr[i]>arr[idx-1] && arr[i]<=arr[prev]){
+                prev=i;
             }
-            Collections.swap(vector , idx-1 , prev);
-            vector= reverePortion(vector , idx-1 , vector.size()-1);
         }
-
-    }
-
-    public static Vector<Integer> reverePortion(Vector<Integer> xyz , int i , int j){
-        Vector<Integer>  reversed = new Vector<>();
-
-        for(int k=0 ;k<i ;k++){
-            reversed.add(xyz.get(k));
-        }
-        for(int k=j ;k>=i ;k--){
-            reversed.add(xyz.get(k));
-        }
-        return reversed;
-
-    }
-
-    public static void main(String[] args) {
-
-        Vector<Integer> nums = new Vector<>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter element : ");
-        String element = sc.next();
-
-        int x;
-        for(int i=0 ;i<element.length();i++){
-             x = Character.getNumericValue(element.charAt(i));
-             nums.add(x);
-        }
-      nextPermutation(nums);
-
-        for(int i=0 ;i< nums.size();i++){
-            System.out.print(nums.get(i));
-        }
-
-
+        swap(arr , idx-1 , prev);
+        revereseArray(arr , idx , arr.length-1);
+        
+        
     }
 }
