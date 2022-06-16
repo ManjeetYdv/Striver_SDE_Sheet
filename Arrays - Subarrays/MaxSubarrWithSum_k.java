578feb95c904f2f45b5685d2ad74bb60f319c8d6
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-16 15:35:51 +0530

    Update MaxSubarrWithSum_k.java

--- Arrays - Subarrays/MaxSubarrWithSum_k.java
+++ Arrays - Subarrays/MaxSubarrWithSum_k.java
@@ -2,11 +2,11 @@ package com.company;
 
 import java.util.HashMap;
 
-public class MaxSubarrayWithSum_k {
+public class maxK {
 
     public static void main(String[] args) {
 
-        int[] arr ={1,2,3};
+        int[] arr ={19 , 3,1  , 1};
         int k=3;
 
         int maxLen=0;
@@ -14,26 +14,19 @@ public class MaxSubarrayWithSum_k {
         int prefix_sum =0;
 
         HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(0 , -1);            //handles cases when prefix_sum==k
 
         for(int i=0 ;i<arr.length ;i++){
 
             prefix_sum +=arr[i];
 
-            if(arr[i]==k && maxLen==0){
-                maxLen =1;
-            }
-            if(prefix_sum ==k ){                             //can be replaced by initialising map.put(0 , -1);
-                maxLen = i+1;
-            }
-
             if(map.containsKey(prefix_sum-k)){
 
                 maxLen = Integer.max(maxLen , i-map.get(prefix_sum-k));
             }
-            else{
+            
+            map.put(prefix_sum , i);
 
-                map.put(prefix_sum , i);
-            }
         }
 
         System.out.println(maxLen);
