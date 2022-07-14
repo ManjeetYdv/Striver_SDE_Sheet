801f65629fa498a366e7fcd0028b31a14a252c23
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-14 23:50:50 +0530

    Update Rat in a Maze Problem - I .java

--- backtracking/Rat in a Maze Problem - I .java
+++ backtracking/Rat in a Maze Problem - I .java
@@ -1,38 +1,34 @@
 class Solution {
     
-  public static void solve(int i, int j, int a[][], int n, ArrayList < String > ans, String move,  int vis[][]) {
+  public static void solve(int i, int j, int a[][], int n, ArrayList < String > ans, String move) {
     if (i == n - 1 && j == n - 1) {
       ans.add(move);
       return;
     }
 
-    // downward
-    if (i + 1 < n && vis[i + 1][j] == 0 && a[i + 1][j] == 1) {
-      vis[i][j] = 1;
-      solve(i + 1, j, a, n, ans, move + 'D', vis);
-      vis[i][j] = 0;
-    }
+
+     a[i][j] = 0;    //marked 0 so that the cell cant be repeated
+     //downward
+    if (i + 1 < n && a[i + 1][j] == 1) { 
+        solve(i + 1, j, a, n, ans, move + 'D');
+      }
 
     // left
-    if (j - 1 >= 0 && vis[i][j - 1] == 0 && a[i][j - 1] == 1) {
-      vis[i][j] = 1;
-      solve(i, j - 1, a, n, ans, move + 'L', vis);
-      vis[i][j] = 0;
-    }
+    if (j - 1 >= 0 && a[i][j - 1] == 1) {
+        solve(i, j - 1, a, n, ans, move + 'L');
+     }
 
     // right 
-    if (j + 1 < n && vis[i][j + 1] == 0 && a[i][j + 1] == 1) {
-      vis[i][j] = 1;
-      solve(i, j + 1, a, n, ans, move + 'R', vis);
-      vis[i][j] = 0;
-    }
+    if (j + 1 < n && a[i][j + 1] == 1) {
+       solve(i, j + 1, a, n, ans, move + 'R');
+     }
 
     // upward
-    if (i - 1 >= 0 && vis[i - 1][j] == 0 && a[i - 1][j] == 1) {
-      vis[i][j] = 1;
-      solve(i - 1, j, a, n, ans, move + 'U', vis);
-      vis[i][j] = 0;
-    }
+    if (i - 1 >= 0  && a[i - 1][j] == 1) {
+      solve(i - 1, j, a, n, ans, move + 'U');
+     }
+    
+     a[i][j] = 1;       //all 4 direction failed for so backtracked and marked it 1 to be visited by others
   }
   
     public static ArrayList<String> findPath(int[][] m, int n) {
@@ -42,8 +38,8 @@ class Solution {
         ArrayList<String> res = new ArrayList<>();
           
        if(m[0][0]==0 || m[n-1][n-1]==0) return res;
-        int[][]vis= new int[n][n];
-        solve(0, 0, m, n,res, "", vis);
+       
+        solve(0, 0, m, n,res, "");
         return res;
     }
 }
