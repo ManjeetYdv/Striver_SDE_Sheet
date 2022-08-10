ed47d42732a71dec20245587c25791b6cd4f026a
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-08-10 22:52:10 +0530

    Update 2Sum.java

--- Arrays Part-4/2Sum.java
+++ Arrays Part-4/2Sum.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        result[0]=0;
-        result[1]=0;
-        int k;
+        
+        
+        HashMap<Integer , Integer> map = new HashMap<>();
+       
         for(int i=0 ;i<nums.length ;i++){
-            int sum = target -nums[i];
-            k=i+1;
-            while(k<nums.length){
-                if(nums[k]==sum){
-                    result[0]=i;
-                    result[1]=k;
-                    return result;
-                }
-                else{
-                    k++;
-                }
+
+            if(map.containsKey(target-nums[i])){
+              return new int[]{i,map.get(target-nums[i])};
             }
+            map.put(nums[i] , i);
         }
-        return result;
+
+        return null;
+
     }
 }
