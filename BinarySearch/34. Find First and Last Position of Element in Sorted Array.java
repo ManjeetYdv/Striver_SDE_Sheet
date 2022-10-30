93bd2a90ba1b7d695d56328abe29b44365d009a1
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-10-31 03:16:41 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.java

--- BinarySearch/34. Find First and Last Position of Element in Sorted Array.java
+++ BinarySearch/34. Find First and Last Position of Element in Sorted Array.java
@@ -1,59 +1,32 @@
 class Solution {
 
-     
+    public int binarySerach(int[]nums , int target){
+        //return leftmost elemen' index is present else return the index where it should be
+        int l=0;
+        int r=nums.length;
+
+        while(l<r){
+            int m = (l+r)/2;
+            if(nums[m]<target) l=m+1;
+            else r =m;
+        }
+
+        return l;
+    }
     public int[] searchRange(int[] nums, int target) {
         
+        if(nums.length==0) return new int[]{-1 , -1};
         int[] res = new int[2];
-        int i =0;
-        int j =nums.length-1;
-        
-        res[0] = -1;
-        res[1] = -1;
-        
-        while(i<=j){
-            
-            int mid = i + (j-i)/2;
-            
-            if(nums[mid] == target){
-                res[0] = mid;
-                j = mid-1;      //to break the loop
-            }
-            else if(nums[mid] > target) j = mid-1;
-            else i = mid+1;
-        }
-        
-        i=0;
-        j=nums.length-1;
-        
-        while(i<=j) {
-            
-            int mid = i + (j-i)/2;
-            
-            if(nums[mid]==target){
-                res[1] = mid;
-                i = mid+1;     //to break the loop
-            }
-            else if(nums[mid] > target) j = mid-1;
-            else i = mid+1;
-            
-        }
-        
+        res[0]=-1;
+        res[1]=-1;
+
+        int first = binarySerach(nums ,target);
+        int last =  binarySerach(nums , target+1)-1;
+
+        if(first<nums.length && nums[first]==target) res[0] = first;
+        if(last>=0 && nums[last]==target) res[1]=last;
+
         return res;
-}
-}
-     // If the target exists, returns its leftmost index.
-    // Else, returns the index of where it should be.
-    // To get the last occurance of element  , return binarySearch(nums , target+1)-1
-     //not using here coz it returns possible position if element dont exist not -1
-    //  public int binarySearch(int[]nums , int target){     
-        
-    //    int l=0;
-    //    int r=nums.length;
        
-    //    while(l<r){
-    //        int m = (l+r)/2;
-    //        if(nums[m]<target) l=m+1;
-    //        else r=m;
-    //    }
-    //    return l;
-    // }
+  }
+}
