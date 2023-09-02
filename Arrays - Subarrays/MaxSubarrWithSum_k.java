3926d98b6f7acbfb5d97f099f48bdaff7cf284da
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2023-09-02 15:52:01 +0530

    Update MaxSubarrWithSum_k.java

--- Arrays - Subarrays/MaxSubarrWithSum_k.java
+++ Arrays - Subarrays/MaxSubarrWithSum_k.java
@@ -33,3 +33,46 @@ public class maxK {
         System.out.println(maxLen);
     }
 }
+
+
+
+//returning longest array with sum k
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class longest_subarr_with_sumK {
+
+    public static List<Integer> maxSubArr(int []arr , int k){
+        HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(0 ,-1);
+        int start =0;
+        int end=0;
+        int len=0;
+        int prefix_sum=0;
+        for(int i=0 ;i<arr.length ;i++){
+            prefix_sum+=arr[i];
+            if(map.containsKey(prefix_sum-k)){
+                int currLen = i - map.get(prefix_sum-k);
+                if(currLen>len){
+                    len=currLen;
+                    start = map.get(prefix_sum-k);
+                    end = i;
+                }
+            }
+            map.put(prefix_sum , i);
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i=start+1 ;i<=end ;i++){
+            list.add(arr[i]);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int[]arr = {1,2,3,1,1,1,1};
+        List<Integer> list = maxSubArr(arr , 3);
+        for(int ele : list) System.out.print(ele+" ");
+    }
+}
